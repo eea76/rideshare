3c676e5f94fe14d6f5bc45c75700de70d22b0621
Elon <elon@Elons-iMac.local>
2020-06-05 11:53:47 -0700

    authentication

--- server/taxi/taxi/settings.py
+++ server/taxi/taxi/settings.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+
 from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -105,4 +107,17 @@ CHANNEL_LAYERS = {
             'hosts': [REDIS_URL]
         },
     },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'USER_ID_CLAIM': 'id',
 }
