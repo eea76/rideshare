58abcd44f211d4bfd88999490e56781992c2b29f
Elon <elon@Elons-iMac.local>
2020-06-08 15:39:00 -0700

    react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link, Route, Switch} from 'react-router-dom';
+
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
+
 import './App.css';
 
 function App() {
     return(
-        <h1>Taxi</h1>
+        <Switch>
+            <Route exact path='/' render={() => (
+                <div>
+                    <h1>Taxi</h1>
+                    <Link to='/sign-up'>Sign up</Link>
+                    <Link to='/log-in'>Log in</Link>
+                </div>
+            )} />
+            <Route path='/sign-up' component={SignUp} />
+            <Route path='/log-in' component={LogIn} />
+        </Switch>
     );
 }
 
