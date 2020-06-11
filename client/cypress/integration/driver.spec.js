eab002bf2ea226054abfa97312414f8ed9274afb
Elon <elon@Elons-iMac.local>
2020-06-10 17:03:57 -0700

    the user

--- client/cypress/integration/driver.spec.js
+++ client/cypress/integration/driver.spec.js
@@ -10,34 +10,34 @@ const logIn = () => {
 };
 
 describe('The driver dashboard', function () {
-  before(function () {
-    cy.loadUserData();
+    before(function () {
+        cy.loadUserData();
   })
 
-  it('Cannot be visited if the user is not a driver', function () {
-    const { username, password } = Cypress.env('rider')
+    it('Cannot be visited if the user is not a driver', function () {
+        const { username, password } = Cypress.env('rider')
 
-    // Capture API calls.
-    cy.server()
-    cy.route('POST', '**/api/log_in/').as('logIn')
+        // Capture API calls.
+        cy.server()
+        cy.route('POST', '**/api/log_in/').as('logIn')
 
-    // Log in.
-    cy.visit('/#/log-in')
-    cy.get('input#username').type(username)
-    cy.get('input#password').type(password, { log: false })
-    cy.get('button').contains('Log in').click()
-    cy.hash().should('eq', '#/')
-    cy.get('button').contains('Log out')
-    cy.wait('@logIn')
+        // Log in.
+        cy.visit('/#/log-in')
+        cy.get('input#username').type(username)
+        cy.get('input#password').type(password, { log: false })
+        cy.get('button').contains('Log in').click()
+        cy.hash().should('eq', '#/')
+        cy.get('button').contains('Log out')
+        cy.wait('@logIn')
 
-    cy.visit('/#/driver')
-    cy.hash().should('eq', '#/')
-  })
+        cy.visit('/#/driver')
+        cy.hash().should('eq', '#/')
+    })
 
-  it('Can be visited if the user is a driver', function () {
-    logIn();
+    it('Can be visited if the user is a driver', function () {
+        logIn();
 
-    cy.visit('/#/driver')
-    cy.hash().should('eq', '#/driver')
-  })
+        cy.visit('/#/driver')
+        cy.hash().should('eq', '#/driver')
+    })
 })
