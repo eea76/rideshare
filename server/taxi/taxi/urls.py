403aa2cfc22d812e8794a1fdea22083612da8855
Elon <elon@Elons-iMac.local>
2020-06-12 16:15:25 -0700

    remove unused imports

--- server/taxi/taxi/urls.py
+++ server/taxi/taxi/urls.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
